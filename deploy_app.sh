049b95a88173f787aa19a85afaada1c8eaee3853
WhereIsMyRum <polcik.piotr@gmail.com>
2020-01-12 00:44:44 +0100

    Fixed deployment script

--- deploy_app.sh
+++ deploy_app.sh
@@ -15,7 +15,7 @@ else
     sudo docker-compose -f $DOCKER_COMPOSE_FILE stop "$DOCKER_IMAGE"
     sudo docker-compose -f $DOCKER_COMPOSE_FILE up -d "$DOCKER_IMAGE"
     sudo docker-compose -f $DOCKER_COMPOSE_FILE exec redis redis-cli flushall || echo "REDIS cache cleared"
-    sudo docker-compose exec nginx /bin/bash /usr/src/scripts/clear-cache.sh || echo "NGINX cache cleared"
+    sudo docker-compose -f $DOCKER_COMPOSE_FILE exec nginx /bin/bash /usr/src/scripts/clear-cache.sh || echo "NGINX cache cleared"
 fi
 
 exit 0

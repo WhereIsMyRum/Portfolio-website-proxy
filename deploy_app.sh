a443f1bd4203e39e538228a87a604b81bac61285
WhereIsMyRum <polcik.piotr@gmail.com>
2020-01-11 23:07:39 +0100

    Added deployment script

---
+++ deploy_app.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+DOCKER_IMAGE=$1
+DOCKER_COMPOSE_FILE="docker-compose.prod.yml"
+
+# Check for arguments
+if [[ $# -lt 1 ]]
+then
+    git pull
+    sudo docker-compose -f $DOCKER_COMPOSE_FILE stop nginx
+    sudo docker-compose -f $DOCKER_COMPOSE_FILE up nginx
+else
+    sudo docker-compose -f $DOCKER_COMPOSE_FILE pull "$DOCKER_IMAGE"
+    sudo docker-compose -f $DOCKER_COMPOSE_FILE stop "$DOCKER_IMAGE"
+    sudo docker-compose -f $DOCKER_COMPOSE_FILE up "$DOCKER_IMAGE" -d
+fi
+
+exit 0

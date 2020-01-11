a758af0cf9cb37d594ec60df56f5e843cfa37afe
WhereIsMyRum <polcik.piotr@gmail.com>
2020-01-12 00:39:47 +0100

    Added cache clearing on fronted/backend updates

---
+++ nginx/clear-cache.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+STATIC_CACHE="/var/cache/nginx/static"
+API_CACHE="/var/cache/nginx/api"
+
+if [[ -f $STATIC_CACHE ]]
+then
+    rm  -r $STATIC_CACHE
+else
+    echo $STATIC_CACHE not found
+fi
+
+if [[ -f $API_CACHE ]]
+then
+    rm  -r $API_CACHE
+else
+    echo $API_CACHE not found
+fi
+
+exit 0

c3904f688bdfdd3524d2e1b92c26b975dde8a89d
WhereIsMyRum <polcik.piotr@gmail.com>
2020-01-12 22:23:07 +0100

    Updated clear-cache script

--- nginx/clear-cache.sh
+++ nginx/clear-cache.sh
@@ -5,14 +5,14 @@ set -e
 STATIC_CACHE="/var/cache/nginx/static/"
 API_CACHE="/var/cache/nginx/api/"
 
-if [[ -f $STATIC_CACHE ]]
+if [[ -e $STATIC_CACHE ]]
 then
     rm  -r $STATIC_CACHE
 else
     echo $STATIC_CACHE not found
 fi
 
-if [[ -f $API_CACHE ]]
+if [[ -e $API_CACHE ]]
 then
     rm  -r $API_CACHE
 else
